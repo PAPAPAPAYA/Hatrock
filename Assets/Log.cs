b54bd30ff2261ccc149e4654c55d34a98f4fef67
PAPAYA <dameningen@126.com>
2021-09-21 21:09:15 -0400

    finished my parts on 战斗方式
    which is 直线和aoe

---
+++ Assets/Log.cs
@@ -0,0 +1,10 @@
+// log
+
+// 9/21/21
+// xmovement
+// xaiming
+// projectile is combined with straight since they are typically the same, the straight line one is actually a projectile
+	// xdestroy spells
+	// xlimit range
+// added aoe that allows the player to choose specific location to cast the spell in a certain range, might come in handy
+	// xrestrict range
