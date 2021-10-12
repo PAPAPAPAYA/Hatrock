26a297a7a6a284b0c284b8ebf6417a844d081b71
Yuhao Gao <harrygao0917@gmail.com>
2021-10-11 20:49:18 -0400

    redone ctrl and drop mat

--- Assets/AyeFolder/AIScripts/Enemy.cs
+++ Assets/AyeFolder/AIScripts/Enemy.cs
@@ -31,6 +31,9 @@ public class Enemy : MonoBehaviour
     public enum AIPhase { NotInBattle, InBattle1, InBattle2 };
     public AIPhase phase;
 
+    public bool attackable;
+    public bool walkable;
+
     private void Awake()
     {
         ghostRider = GetComponent<NavMeshAgent>();
